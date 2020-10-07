64cebc900f135f251ead0002b6608e4cf806cdae
Matias-Louzao-Ataria <louzao.ataria.matias@gmail.com>
2020-10-07 13:04:02 +0200

    Primeros ejercicios

---
+++ MyApplication/app/src/main/java/prueba/primerclase/MainActivity.java
@@ -0,0 +1,31 @@
+package prueba.primerclase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        System.out.println("a");
+    }
+}

5103a49891a998c9f255c3260e5b9d6ee5eef85e
Matias-Louzao-Ataria <louzao.ataria.matias@gmail.com>
2021-02-10 18:10:08 +0100

    Implementacion de la camara

--- BouncingBullets/android/src/com/matias/bouncingbullets/AndroidLauncher.java
+++ BouncingBullets/android/src/com/matias/bouncingbullets/AndroidLauncher.java
@@ -1,11 +1,20 @@
 package com.matias.bouncingbullets;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.matias.bouncingbullets.Main;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class AndroidLauncher extends AndroidApplication {
 	/**
@@ -18,6 +27,31 @@ public class AndroidLauncher extends AndroidApplication {
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		config.useImmersiveMode = true;
 		config.useWakelock = true;
-		initialize(new Main(new CamaraAndroid(this)));
+		/*Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);*/
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+			initialize(new Main(new CamaraAndroid(this)));
+		}
+	}
+
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		Log.i("BBBB","BBBB");
+//                ImageView iv = (ImageView) findViewById(R.id.imageView);
+		if (requestCode == 1 && resultCode == RESULT_OK) { // Thumbnail
+			Bitmap imagen = (Bitmap) data.getExtras().get("data");
+//            iv.setImageBitmap(imagen);
+			try { // Guardo la imagen
+				FileOutputStream fout = openFileOutput("imagen2.jpg", MODE_PRIVATE);
+				imagen.compress(Bitmap.CompressFormat.JPEG, 80, fout);
+				fout.close();
+				String[] files = getApplicationContext().fileList(); // Visualizo el nombre de las imagenes guardadas
+				for (String file : files) {
+					Log.i("imagen -> ", file);
+					BitmapFactory.decodeFile(file);
+				}
+			} catch (IOException e) {
+				Log.e("Error imagen", e.getLocalizedMessage());
+			}
+		}
 	}
 }
--- BouncingBullets/android/src/com/matias/bouncingbullets/CamaraAndroid.java
+++ BouncingBullets/android/src/com/matias/bouncingbullets/CamaraAndroid.java
@@ -1,23 +1,24 @@
 package com.matias.bouncingbullets;
 
-/*import android.Manifest;
-import android.annotation.TargetApi;
+
+import android.Manifest;
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.hardware.camera2.*;
+import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraDevice;
-import android.icu.text.SimpleDateFormat;
-import android.net.Uri;
+import android.hardware.camera2.CameraManager;
 import android.os.Build;
-import android.os.Environment;
+import android.os.storage.StorageManager;
 import android.provider.MediaStore;
-import androidx.annotation.RequiersApi;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.util.Log;
-import android.view.View;
-import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -26,27 +27,35 @@ import java.io.IOException;
 import java.util.Date;
 
 @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-public class CamaraAndroid extends Activity implements PlatformSpecific {
+public class CamaraAndroid extends AndroidApplication implements PlatformSpecific {
 
     private Activity context;
-    CameraDevice c;
-    CameraDevice.StateCallback stateCallback;
 
     public CamaraAndroid(Activity context) {
         this.context = context;
     }
 
+    public CamaraAndroid() {
+    }
+
     public void hacerFoto() {
-        this.context.runOnUiThread(new Runnable() {
+
+//        Intent i = new Intent().setAction(Intent.ACTION_GET_CONTENT);
+        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //startActivityForResult(i,1);
+        this.context.startActivityForResult(Intent.createChooser(i,"select"),1);
+        /*this.context.runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                Toast.makeText(context, "Hey platform specific code!", Toast.LENGTH_SHORT).show();
+
+                /*Toast.makeText(context, "Hey platform specific code!", Toast.LENGTH_SHORT).show();
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
                     if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
                         stateCallback = new CameraDevice.StateCallback() {
                             @Override
                             public void onOpened(@NonNull CameraDevice camera) {
                                 camera = camera;
+
                             }
 
                             @Override
@@ -61,54 +70,10 @@ public class CamaraAndroid extends Activity implements PlatformSpecific {
                                 camera = null;
                             }
                         };
+                        abrirCamara();
                     }
                 }
             }
-        });
+        });*/
     }
-
-
-    public void abrirCamara(){
-        CameraManager manager = null;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            manager = (CameraManager) getSystemService(context.CAMERA_SERVICE);
-        }
-        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            try {
-                manager.openCamera("camera", stateCallback, null);
-            } catch (CameraAccessException e) {
-                System.err.println(e.getLocalizedMessage());
-            }
-        }
-    }
-
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-//        ImageView iv = (ImageView) findViewById(R.id.imageView);
-        if (requestCode == 1 && resultCode == RESULT_OK) { // Thumbnail
-            Bitmap imagen = (Bitmap) data.getExtras().get("data");
-//            iv.setImageBitmap(imagen);
-            try { // Guardo la imagen
-                FileOutputStream fout = openFileOutput("imagen.jpg", MODE_PRIVATE);
-                imagen.compress(Bitmap.CompressFormat.JPEG, 80, fout);
-                fout.close();
-                String[] files = getApplicationContext().fileList(); // Visualizo el nombre de las imagenes guardadas
-                for (String file : files) {
-                    Log.i("imagen -> ", file);
-                }
-            } catch (IOException e) {
-                Log.e("Error imagen", e.getLocalizedMessage());
-            }
-        }
-    }
-}*/
+}
